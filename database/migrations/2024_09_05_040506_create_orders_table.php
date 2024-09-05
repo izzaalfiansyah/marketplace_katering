8cc275332c10db595710115bdd8a3591ed738ed7
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2024-09-05 11:10:28 +0700

    add menus & orders table

---
+++ database/migrations/2024_09_05_040506_create_orders_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->timestamp('schedule');
+            $table->enum('status', ['pending', 'process', 'completed', 'canceled'])->default('pending');
+            $table->text('reason')->nullable();
+            $table->integer('total');
+            $table->integer('payment');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
